a358286f8296e4945719eb0b9929f3a062273f7a
alexisFdz19 <alexisFdz_19@outlook.com>
2020-09-01 19:42:03 -0500

    Error 404

---
+++ views/modulos/default/404.php
@@ -0,0 +1,31 @@
+<!-- hero slider -->
+
+<section class="hero-section overlay bg-cover" data-background="<?php echo $url?>views/images/banner/banner.jpg">
+
+  <div class="container">
+
+    <div class="hero-slider">
+
+      <!-- slider item -->
+
+      <div class="hero-slider-item">
+
+        <div class="row">
+
+          <div class="col-md-8">
+
+            <a href="<?php echo $url?>"><h1 class="text-white">Error 404</h1></a>  
+
+            <p class="text-muted mb-4"  style="text-align: justify;">Parece que la página que estás buscando no existe
+            </p>
+
+            <a href="<?php echo $url?>" class="btn btn-primary">Regresar</a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+</section>
+<!-- /about us -->
