fcb2f49690e01a268bf4d72e6174d8b6542cd020
alexisFdz19 <alexisFdz_19@outlook.com>
2020-09-01 22:48:48 -0500

    Gracias

--- views/php/send_mail.php
+++ views/php/send_mail.php
@@ -5,12 +5,14 @@
 	$nombre = $_POST["name"];
 	$correo = $_POST["email"];
 	$mensaje = $_POST["message"];
+	
+	// Junta los datos
 	$contenido = "Nombre: " .$nombre. "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
 
 	// Envía
-	mail($destino, "contacto", $contenido);
+	mail($destino, "Contacto CIA Elemental - Página Web", $contenido);
 
 	// Redirecciona
-	header("Location:<?php echo $url?>/views/modulos/contacto.php");
+	header("Location:correo-enviado.php");
 
 ?>
